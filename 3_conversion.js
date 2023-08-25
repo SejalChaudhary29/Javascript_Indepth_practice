24209833449437356513f13858ecf8aea17ed857
SejalChaudhary29 <sejalch2933@gmail.com>
2023-08-25 14:35:59 +0530

    conersion of datatype

---
+++ 3_conversion.js
@@ -0,0 +1,37 @@
+ let score = 234; 
+ let scoreTwo =  '123abc'
+ console.log(typeof score)
+ console.log(typeof scoreTwo)
+
+
+ /* surety is here it will convert data type as a NUMBER */
+ let valueInNumber = Number(scoreTwo)
+
+ console.log(typeof valueInNumber) 
+ console.log(valueInNumber) 
+
+
+ /* 
+ conversion in number
+ '22' => 22
+ '22abc' => NAN
+ true => 1 ; false => 0  
+ undefined => NAN
+ null => 0
+ */
+
+/* 
+conversion in boolean
+
+1 => true
+0 => false 
+'' => false
+'hello' => true
+
+*/ 
+/* 
+conversion in string
+ null => null 
+undefined => undefined
+*/
+ 
