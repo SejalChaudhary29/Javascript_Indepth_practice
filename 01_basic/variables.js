84e2966c208f529cd412a4c8ba180e5b5660c0fa
SejalChaudhary29 <sejalch2933@gmail.com>
2023-08-25 12:02:51 +0530

    variable done

---
+++ 01_basic/variables.js
@@ -0,0 +1,21 @@
+
+// let, const, and var  
+/* let can be updated and not re-declared 
+   var can be updated and can be re-declared
+   const cannot be updated and re-declared
+*/
+const accountId = 123455; 
+let accountEmail = 'sejalch@twitter.com';
+var password = '1234@67';
+accountCity = 'jaipur'
+
+accountCity = 'indore'
+accountEmail = 'sam@gmail.com' 
+password = '124759'
+
+
+/*
+accountId = 2333  TypeError: Assignment to constant variable
+ accountEmail = 'sam@gmail.com' let can be updated
+password = '124759'  var can update */
+console.table( [accountCity, accountId , accountEmail ,  password])
