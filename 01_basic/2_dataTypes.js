44970c84210fa21cf544210a433debb4ada4e4a9
SejalChaudhary29 <sejalch2933@gmail.com>
2023-08-25 12:55:38 +0530

    datatype

---
+++ 01_basic/2_dataTypes.js
@@ -0,0 +1,34 @@
+'use strict' ;  //treat all js code as newer version 
+
+
+let name =  'sejal'
+let age = 18 
+let isLoggedIn = true;
+
+/*
+Primitive datatype
+number  2 to power 53 
+string  ''
+BigInt high value 
+boolean true/ false 
+null  standalone value not a zero (0)
+undefined  not defined the value 
+symbol use to find a uniqueness
+
+*/
+ /* 
+ type of datatype
+
+  string => string 
+  number => number
+  boolean => boolean
+  null = > object 
+  undefined => undefined
+ */
+
+/*
+Non-primitive datatype
+object
+array 
+function 
+*/
