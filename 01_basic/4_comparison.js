6ab2b2da79093e3ae2dea098c3c97c80dc6cb216
SejalChaudhary29 <sejalch2933@gmail.com>
2023-08-26 11:13:46 +0530

    comparision

---
+++ 01_basic/4_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1)
+// console.log(2==2)
+
+// story of equality check(==) and comparision check(>=, =<) 
+// both of the check acts differntly
+console.log(null > 0)//false
+console.log(null == 0 )//false
+console.log(null>=0)//true >>>>>in comparision null becomes as 0 
+//  >>>>>>>>>>>>undefined always gives false in both cases<<<<<<<<<<<<<<<<<<<<<<<<< 
+ console.log(undefined > 0 ) //false
+ console.log(undefined >= 0 )//false
+ console.log(undefined == 0 )//false
+
+
+ /*  STRICT CHECK (===)*/
+ console.log('2' === 2) // false , it checks datatypes
