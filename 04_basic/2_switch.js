daae6207f35b96e1e81513f5ef74237adf7d67a9
SejalChaudhary29 <sejalch2933@gmail.com>
2023-09-05 20:41:03 +0530

    donw with switch statment

---
+++ 04_basic/2_switch.js
@@ -0,0 +1,41 @@
+//switch case statement 
+
+const month = 'May'
+switch (month) {
+    case 'Jan':
+        console.log('January')
+        
+        break;
+    case 'Feb':
+        console.log('February')
+        
+        break;
+    case 'Mar':
+        console.log('March')
+        
+        break;
+    case 'Apr':
+        console.log('April')
+        
+        break;
+    case 'May':
+        console.log('May month matched')
+        
+        break;
+    case 'Jun':
+        console.log('June month matched')
+        
+        break;
+    case 'July':
+        console.log('July month matched')
+        
+      break;
+
+    default:
+        console.log('Not found')
+        break;
+}
+
+// Note: if you didnt use break keyword. the matched value with other gets executed except the default execution .  
+
+
