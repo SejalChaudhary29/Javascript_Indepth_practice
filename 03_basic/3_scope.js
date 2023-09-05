06be1653f9c01658ccb150cdbd1b4ae4f1b8d43f
SejalChaudhary29 <sejalch2933@gmail.com>
2023-09-05 10:52:48 +0530

    done with scope -let var and const

--- 03_basic/3_scope.js
+++ 03_basic/3_scope.js
@@ -1,9 +1,17 @@
-let a = 10
+// var a = 200;
+let a = 10;
+
 if(true){
+    // var a = 500;
+    // console.log(a) //500
+
+    // <<<<<<let and const >>>>>>>>>>
+    
     let a = 400;
     console.log(a)//400
 
 }
+// console.log(a) //500 
 console.log(a)//10
 
 //let and const are block scope
