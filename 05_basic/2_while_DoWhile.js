9be707d8a226c6c860102128a0e047b826e7d2e9
SejalChaudhary29 <sejalch2933@gmail.com>
2023-09-06 13:56:55 +0530

    while and do while loop

---
+++ 05_basic/2_while_DoWhile.js
@@ -0,0 +1,23 @@
+//while
+let i = 1
+while(i<=10){
+    console.log(' while loop value is i ', i)
+    i+=3
+
+}
+const myArray = ['batman', 'spiderman', 'blackman']
+
+    let j = 0
+while(j < myArray.length){
+    console.log('heros in array', myArray[j])
+    j++
+}
+
+//do while
+
+let score = 10; 
+do{
+    console.log('do while loops starts', score)
+    score++
+
+}while(score <=5) //do while loops starts 10
