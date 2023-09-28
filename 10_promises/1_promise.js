da81d23a2c71afe3e6110397a8112579c2e824b7
SejalChaudhary29 <sejalch2933@gmail.com>
2023-09-28 12:13:26 +0530

    promise

---
+++ 10_promises/1_promise.js
@@ -0,0 +1,55 @@
+// promise instance
+
+const promiseOne = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Async task is compeleted");
+    resolve();
+  }, 1000);
+});
+
+promiseOne.then(function () {
+  console.log("promise consumed");
+});
+
+const promiseTwo = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let error = false;
+    if (!error) {
+      resolve({username:'sejal'});
+    } else {
+      reject("I am a reject");
+    }
+  }, 1000);
+});
+
+promiseTwo.then((username)=>{
+  console.log( username ,'resolvr is consumed')
+}).catch((error)=>{
+  console.log(error)
+}).finally(()=>{
+  console.log('the promise either resolved or rejected')
+})
+
+//async await with try and catch
+
+const promiseThree = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let error = false; // Set error to false to resolve the promise
+    if (!error) {
+      resolve({ username: 'hitesh' });
+    } else {
+      reject("I am a reject");
+    }
+  }, 1000);
+});
+
+async function consumePromise() { 
+  try {
+    const response = await promiseThree; 
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+consumePromise(); 
